ff7847c035ef664c615e513c87da0ca117ae5009
so-slay <s.vananthakrishna@students.iiserpune.ac.in>
2026-02-11 23:36:48 +0530

    Wednesday: Go solo?

--- markovNull.py
+++ markovNull.py
@@ -2,7 +2,7 @@ from pathlib import Path
 import numpy as np 
 import pandas as pd 
 import GetFASTAfromTSV as gf 
-from collections import Counter
+from collections import Counter, defaultdict
 
 header = ['chr', 'start', 'stop', 'ATAC', 'CTCF', 'REST', 'EP300', 'sequence' ]
 
@@ -11,7 +11,7 @@ header = ['chr', 'start', 'stop', 'ATAC', 'CTCF', 'REST', 'EP300', 'sequence' ]
 # Inputs: .bed & .fa files with sequences 
 # Basics: Read the bed files, and make a data frame
 # Append the sequence info from corresponding FASTA file
-# Handle "Ns"
+# Handle "NaNs"
 
 # Choose one TF
 # "DO you want to use ATAC info?" > Select only B in ATAC and then train
@@ -32,7 +32,7 @@ for bed in bed_files:
 
 # Data structuring: list of dfs
 # Each sub df is a combination of bed and fasta info.
-chrom_dfs = []
+dict_of_dfs = {}
 for bed, fasta in bed_fasta_dict.items():
     df_bed = pd.read_csv(bed, sep="\t", header=0)
     df_fasta = pd.read_csv(fasta, header =None)
@@ -40,17 +40,70 @@ for bed, fasta in bed_fasta_dict.items():
     df = pd.concat([df_bed, df_fastest], axis =1)
     df.columns = header
     print(f'bed: {bed}; fasta: {fasta} concatenated to give: {df}')
-    chrom_dfs.append(df)
+    # Create a dictionary so dfs can be named
+    dict_of_dfs[Path(bed).stem] = df
 
 # Note: for a very robust pipeline, the lines 0,2,4... of the fasta file can be used to map to the right bin in bed. (perhaps later)
 
+bind_Boolmap = {"B":1, "U":0}
+cols_to_Bool = ["ATAC", "CTCF", "REST", "EP300"]
 
-# Count using Counter for 0 order markov model:
-for df in chrom_dfs:
-    df['counts'] = df['sequence'].apply(Counter)
-# Comprehension version of the same as above:
-# chrom_dfs = [df.assign(counts=df['sequence'].apply(Counter)) for df in chrom_dfs]
-print(chrom_dfs)
+for k, df in dict_of_dfs.items():
+    # Set mask to ignore NaNs
+    df['unmapped'] = df.iloc[:, 0].isna() # Bool 1 for NaN
+    mask = ~df['unmapped'] # Bool 0 for NaN, 1 for mapped regions
+
+    # Convert U B to Boolean with bind_Boolmap
+    df[cols_to_Bool] = df[cols_to_Bool].apply(lambda col: col.map(bind_Boolmap))
+
+    # Count using Counter for 0-order markov model:
+    df.loc[mask, 'counts'] = df.loc[mask, 'sequence'].apply(Counter)
+
+    # Convert counts to probabilities:
+    df.loc[mask, "probs"] = df.loc[mask, "counts"].apply(lambda c: {k: v/sum(c.values()) for k,v in c.items()})
+
+# Test code:
+# print(chrom_dfs[0].iloc[3])
+
+## Markov Model of Order 0: Calculate nucleotide frequencies (background) for bound, unbound
+which_factor = "CTCF"
+# which_factors = gf.config["which_factor"]
+# atac = gf.config["USE ATAC info?"]
+
+nucleotides = ["A", "C", "G", "T"]
+
+bg_stats = {k:{"bound":pd.Series(dtype=float) , "unbound":pd.Series(dtype=float)}for k in dict_of_dfs.keys()}
+
+for k, df in dict_of_dfs.items():    
+    # Make ATCG counts a df
+    df = df[df["probs"].apply(lambda x: isinstance(x, dict))] # drop None instances
+    prob_df = df["probs"].apply(lambda d: {nt: d.get(nt, 0) for nt in nucleotides}).apply(pd.Series)
+    
+    # Create a mask from parent df:
+    bound_mask  = df[which_factor] == 1    
+
+    # Calculate averages for bound, unbound
+    bound_avg = prob_df[bound_mask].mean(axis=0) 
+    unbound_avg = prob_df[~bound_mask].mean(axis=0) 
+    bg_stats[k] = {"bound":bound_avg, "unbound":unbound_avg}
+    print(f"{df} stats: Bound Probs: {bound_avg} , Unbound Probs:{unbound_avg}")
+
+
+# Define a k-th order markov model fn, where k is an integer from 1 to 10
+# k = gf.config["MarkovOrder"]
+k = int(3)
+
+# Build a function to work on any sequence string
+# Iterate over list of sequences
+# do this in bound/unbound sets for specified factor
+
+def markov_ll(seq):
+    
+    for seq in seqlist:
+
+
+
+
 
 
 #markov_null 
